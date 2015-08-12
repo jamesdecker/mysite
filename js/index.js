ee1ca2b6000657b7de7c9d3c18bfa617187a7de3
jamesdecker <james@thinkdamnit.com>
2015-08-12 19:42:28 -0400

    added accordion function
    Needs more work and notation, but progress

--- js/index.js
+++ js/index.js
@@ -1,38 +1,86 @@
-$(document).ready(function(){
+jQuery(document).ready(function() {
+	function close_accordion_section() {
+		jQuery('.accordion .accordion-section-title').removeClass('active');
+		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+	}
 
-  function getInput() {
-    event.preventDefault();
-    var city = $("#city-type").val();
- // 2. defines the variable "city" by the input for ID city-type.
+	jQuery('.accordion-section-title').click(function(e) {
 
-    if (city == "New York City" || city == "NYC" || city =="nyc") {
-      $("body").removeClass();
-      $("body").addClass("nyc");
-    }
- // 3. then runs the program, first clearing the class and then passing in a new class into "body" 
+    // Grab current anchor value
+		var currentAttrValue = jQuery(this).attr('href');
 
-    else if (city == "Austin" || city =="austin") {
-      $("body").removeClass();
-      $("body").addClass("austin");
-    }
+		if(jQuery(e.target).is('.active')) {
+			close_accordion_section();
+		}else {
+			close_accordion_section();
 
-    else if (city == "Los Angeles" || city =="la") {
-      $("body").removeClass();
-      $("body").addClass("la");
-    }
+			// Add active class to section title
+			jQuery(this).addClass('active');
 
-    else if (city =="San Francisco" || city =="sf") {
-      $("body").removeClass();
-      $("body").addClass("sf");
-    }
+			// Open up the hidden content panel
+			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+		}
 
-    else if (city =="Sydney" || city =="sydney") {
-      $("body").removeClass();
-      $("body").addClass("sydney");
-    }
-  }
-
-  $("#submit-btn").click(getInput);
- // 1. On click, run the program getInput
-
+		event.preventDefault();
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   function getInput() {
+//     event.preventDefault();
+//     var city = $("#city-type").val();
+//  // 2. defines the variable "city" by the input for ID city-type.
+//
+//     if (city == "New York City" || city == "NYC" || city =="nyc") {
+//       $("body").removeClass();
+//       $("body").addClass("nyc");
+//     }
+//  // 3. then runs the program, first clearing the class and then passing in a new class into "body"
+//
+//     else if (city == "Austin" || city =="austin") {
+//       $("body").removeClass();
+//       $("body").addClass("austin");
+//     }
+//
+//     else if (city == "Los Angeles" || city =="la") {
+//       $("body").removeClass();
+//       $("body").addClass("la");
+//     }
+//
+//     else if (city =="San Francisco" || city =="sf") {
+//       $("body").removeClass();
+//       $("body").addClass("sf");
+//     }
+//
+//     else if (city =="Sydney" || city =="sydney") {
+//       $("body").removeClass();
+//       $("body").addClass("sydney");
+//     }
+//   }
+//
+//   $("#submit-btn").click(getInput);
+//  // 1. On click, run the program getInput
+//
+// });
