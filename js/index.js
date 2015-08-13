a69812855ff5ebf4e870686612df6a9323f1f6df
jamesdecker <james@thinkdamnit.com>
2015-08-12 23:09:22 -0400

    Adding responsive features

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function() {
 	}
 
 	jQuery('.accordion-section-title').click(function(e) {
-
-    // Grab current anchor value
+		event.preventDefault();
+    // Grabs current anchor value
 		var currentAttrValue = jQuery(this).attr('href');
 
 		if(jQuery(e.target).is('.active')) {
@@ -14,14 +14,13 @@ jQuery(document).ready(function() {
 		}else {
 			close_accordion_section();
 
-			// Add active class to section title
+			// Adds active class to section title
 			jQuery(this).addClass('active');
 
 			// Open up the hidden content panel
 			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
 		}
-
-		event.preventDefault();
+	
 	});
 });
 
