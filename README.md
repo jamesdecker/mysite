fa1d7d63610ae4c626fb882dd1dab4a9c49a4ced
jamesdecker <james@thinkdamnit.com>
2015-08-24 14:47:47 -0400

    adding more

--- README.md
+++
@@ -1,3 +0,0 @@
-I've made an edit to this file.
-I've made another edit.
-And another. # mysite
--- js/index.js
+++ js/index.js
@@ -23,6 +23,34 @@ jQuery(document).ready(function() {
 		}
 
 	});
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1500)
+    .next()
+    .fadeIn(1500)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
+
+$("#slideshowOne > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshowOne > div:first')
+    .fadeOut(1500)
+    .next()
+    .fadeIn(1500)
+    .end()
+    .appendTo('#slideshowOne');
+},  5000);
+
+
+
+
+
+
 });
 
 
